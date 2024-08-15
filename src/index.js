97da901b9fc1a2f0a3286db5f0f6ebee99873479
Marco Starker <marco.starker@gmx.net>
2024-08-15 16:29:53 +0200

    fix: locale number

--- src/index.js
+++ src/index.js
@@ -10,15 +10,18 @@ const options = commandLineArgs([
     {name: 'file', alias: 'f', type: String, defaultValue: process.env.K2C_FILE},
     {name: 'filterSport', type: String, description: 'Filter by type sport. E.g. touringbicycle', defaultValue: process.env.K2C_FILTER_SPORT ?? undefined},
     {name: 'filterAfterDate', type: String, description: 'Filter by date and ignore all before given date. E.g. 2021-08-13T15:20:20.000Z', defaultValue: process.env.K2C_FILTER_AFTER_DATE ?? undefined},
+    {name: 'locale', type: String, description: 'Locale to use for output. Default de-DE', defaultValue: process.env.K2C_LOCALE ?? 'de-DE'},
 ]);
-if (options.filterAfterDate !== undefined) {
-    options.filterAfterDate = new Date(options.filterAfterDate);
+
+Number.prototype.n2s  = function() {
+    return this.toLocaleString(options.locale, {minimumFractionDigits: 0, maximumFractionDigits: 12});
 }
 
 const client = new Client({...options});
 
 console.log('Requesting tours');
-const tours       = await client.toursMade();
+const tours     = await client.toursMade({sportTypes: options.filterSport, startDate: options.filterAfterDate});
+
 let speedTotal    = 0;
 let distanceTotal = 0;
 let durationTotal = 0;
@@ -27,47 +30,31 @@ const result = [
     [
         'Id', 'Name', 'Date',
 
-        'Speed', 'Ø Speed',
+        'Ø Speed', 'Speed',
         'Total Distance (km)', 'Ø Distance (km)', 'Distance (km)',
-        'Total Duration (h)', 'Ø Duration (h)', 'Duration (h)', 'Duration with Pause (h)',
+        'Total Duration (h)', 'Ø Duration (h)', 'Duration (h)',
 
         'Sport',
-        'Elevation Up (m)', 'Elevation Down (m)',
+        'Elevation Up (m)', 'Elevation Down (m)', 'Elevation Normalized',
         'Latitude', 'Longitude', 'Altitude'
     ].join(';')
-].concat(
-    tours
-        .filter((tour) => {
-            if (options.filterSport !== undefined && tour.sport !== options.filterSport) {
-                return false;
-            }
+].concat(tours.map((tour, index) => {
+    speedTotal += tour.speed.inMotion;
+    distanceTotal += tour.distance;
+    durationTotal += tour.duration.inMotion;
 
-            return !(options.filterAfterDate !== undefined && tour.date < options.filterAfterDate);
-        })
-        .sort((a, b) => a.date - b.date)
-        .map((tour, index) => {
-            const distance          = tour.distance / 1000;
-            const duration          = (tour.duration.inMotion ?? tour.duration.total) / 60 / 60;
-            const durationWithPause = tour.duration.total / 60 / 60;
-            const speed             = distance / duration;
+    return [
+        tour.id, tour.name, dayjs(tour.date).format('DD.MM.YYYY HH:mm:ss'),
 
-            speedTotal += speed;
-            distanceTotal += distance;
-            durationTotal += duration;
+        (speedTotal / (index + 1)).n2s(), tour.speed.inMotion.n2s(),
+        distanceTotal.n2s(), (distanceTotal / (index + 1)).n2s(), tour.distance.n2s(),
+        durationTotal.n2s(), (durationTotal / (index + 1)).n2s(), tour.duration.inMotion.n2s(),
 
-            return [
-                tour.id, tour.name, dayjs(tour.date).format('DD.MM.YYYY HH:mm:ss'),
-
-                speed, speedTotal / (index + 1),
-                distanceTotal, distanceTotal / (index + 1), distance,
-                durationTotal, durationTotal / (index + 1), duration, durationWithPause,
-
-                tour.sport,
-                tour.elevation.up, tour.elevation.down,
-                tour.startPoint.lat, tour.startPoint.lng, tour.startPoint.alt,
-            ].join(';');
-        })
-);
+        tour.sport,
+        tour.elevation.up.n2s(), tour.elevation.down.n2s(),
+        tour.startPoint.lat.n2s(), tour.startPoint.lng.n2s(), tour.startPoint.alt.n2s(),
+    ].join(';');
+}));
 
 fs.writeFileSync(options.file, result.join('\n'), {encoding: 'utf8'});
 console.log('Done');
